f03580dd4c962424443c3b26d851fe8d07834874
SternHillpocken <nicolas.guerin.pro@laposte.net>
2023-12-28 19:09:44 +0100

    Local storage checkup for highlighted button : background > tutorial > play

--- src/app/components/ui/home/home-choices/home-choices.component.ts
+++ src/app/components/ui/home/home-choices/home-choices.component.ts
@@ -10,8 +10,17 @@ export class HomeChoicesComponent {
   @Output()
   buttonSelectionEmitter: EventEmitter<"loadout" | "tutorial" | "background"> = new EventEmitter();
 
+  localStorage: string = "";
+
+  ngOnInit(): void {
+    if (!window.localStorage.getItem("kalama")) window.localStorage.setItem("kalama", "background");
+    this.localStorage = window.localStorage.getItem("kalama") as string;
+  }
+
   buttonSelect(name: "loadout" | "tutorial" | "background"): void {
     this.buttonSelectionEmitter.emit(name);
+    if (name === "background") window.localStorage.setItem("kalama", "tutorial");
+    else if (name === "tutorial") window.localStorage.setItem("kalama", "play")
   }
 
 }
