e3bf3cd8332421823c3e817bb8a5f0d95095fe99
Nicolas Guérin <nicolas.guerin.pro@laposte.net>
2024-01-17 14:39:15 +0100

    Powers as new object

---
+++ src/app/models/power.model.ts
@@ -0,0 +1,8 @@
+export class Power {
+    constructor(
+        public name: string,
+        public title: string,
+        public description: string,
+        public maxPowerCoolDown: number
+    ){}
+}
